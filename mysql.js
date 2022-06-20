98bb6c1b871b6249133673ef4bee30d8087380fb
Diego Davila <diego.davilafreitas@gmail.com>
2022-06-20 19:29:14 +0200

    conection succesfully

--- mysql.js
+++ mysql.js
@@ -15,4 +15,19 @@ conectionUser.connect((err)=>{
         console.log("La conexión funciona");
     }
 })
+//Para hacer consultas a la bbdd:
+conectionUser.query("SELECT * FROM user", (err, rows)=>{
+    if(err){
+        throw err;
+    }else{
+        console.log('los datos de la tabla son estos:');
+        console.log(rows)
+        var user1 = rows[0];
+        console.log(`el usuario se llama ${user1.nombre}`);
+        rows.forEach(element => {
+            console.log("Nombre: "+element.nombre)
+        });
+    }
+});
+// Recuerda cerrar la conexión despues de hacer las querys
 conectionUser.end();
